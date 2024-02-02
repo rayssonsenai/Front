08df4a038b0f10b9117775072cf93ac2b03e7915
Lord <rayssonsenai@gmail.com>
2024-02-02 17:27:24 -0300

    Update exmy2.js

--- Cursinho_FullStack/Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 02/exp2/exmy2.js
+++ Cursinho_FullStack/Módulo 02/07 - Interação com API/Aula 01 - Pré-API/Exemplo 02/exp2/exmy2.js
@@ -1,10 +1,15 @@
-function mais() {
-    return (((4234234234+12331)/3123)-1355823)*2
-} 
-alert (mais().toFixed(2))
-if (mais()>10){
-    alert(mais().toFixed(2)+ ' e maior quê 10.')
+// function mais() {
+//     return (((4234234234+12331)/3123)-1355823)*2
+// } 
+// alert (mais().toFixed(2))
+// if (mais()>10){
+//     alert(mais().toFixed(2)+ ' e maior quê 10.')
+// }
+// else{
+//     alert(mais().toFixed(2)+ ' e menor quê 10.')
+// }
+
+function somar(y,x) {
+    return parseInt(y)+parseInt(x)
 }
-else{
-    alert(mais().toFixed(2)+ ' e menor quê 10.')
-}
+alert(somar(prompt(),prompt()))
