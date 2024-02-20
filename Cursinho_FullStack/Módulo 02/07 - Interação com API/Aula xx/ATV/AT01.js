02573b1b2553c8571ab1d4e64db1dd77e60ebe66
Lord <rayssonsenai@gmail.com>
2024-02-20 17:39:12 -0300

    20/02/24

---
+++ Cursinho_FullStack/Módulo 02/07 - Interação com API/Aula xx/ATV/AT01.js
@@ -0,0 +1,13 @@
+async function nsei() {
+    let look = await fetch("AT01.json")
+    let cont = await look.json()
+
+    let lookagain = await fetch("ATC01.json")
+    let cl = await lookagain.json()
+
+    for (let c in cl){
+    document.body.innerHTML+=`<h1 style:"color: ${cl[c]}">${cont[c]}</h1>`
+    
+}
+}
+nsei()
--- Cursinho_FullStack/Módulo 02/07 - Interação com API/Aula xx/Exp 02/exemplo-02.js
+++ Cursinho_FullStack/Módulo 02/07 - Interação com API/Aula xx/Exp 02/exemplo-02.js
@@ -1,9 +1,17 @@
 async function apareça(){
     let ache = await fetch("exemplo-02.json")
     let missao = await ache.json()
-    for (let x in missao){
+
+    let achednv = await fetch("exemplo-02_C.json")
+    let cor = await achednv.json()
+    // for (let x in missao){
+    //     document.body.innerHTML+=`
+    //         <h1>${missao[x]}</h1> ${x}`
+    // }
+    for (let x of missao){
+        let qualquercor=parseInt(Math.random()*5 )
         document.body.innerHTML+=`
-            <h1>${missao[x]}</h1> ${x}`
+            <h1 style="color:${cor[qualquercor]}"${x}</h1>`
     }
 }
 apareça()
