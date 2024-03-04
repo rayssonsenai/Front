a4fe62a58bcd811d0e2f3da7dfea4276f5ff3ebc
Lord <rayssonsenai@gmail.com>
2024-03-04 17:28:12 -0300

    04/03/24

---
+++ Cursinho_FullStack/Módulo 02/07 - Interação com API/Final/fim.js
@@ -0,0 +1,18 @@
+async function dnvnsei(){
+    let vejala = await fetch("fim.json")
+    let achou = await vejala.json()
+    
+    let vaila = document.getElementById("akioh")
+    
+    for(let xyz of achou){
+        vaila.innerHTML+=
+            `<div class="anc">
+                <img src=${xyz.img} width=auto height=400px>
+                <h2>${xyz.modelo}</h1>
+                <p>${xyz.marca}</p>
+                <p>Ano: ${xyz.ano}</p>
+                <p>R$ ${xyz.valor.toFixed(2)}</p>
+            </div>`
+    }
+}
+dnvnsei()
